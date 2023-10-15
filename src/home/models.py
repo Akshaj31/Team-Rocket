f5918854f99628023216e6d049131ac7fd082892
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-10-15 21:31:39 +0530

    add Employee model & specify schema

--- src/home/models.py
+++ src/home/models.py
@@ -2,10 +2,26 @@ from django.db import models
 
 
 # Create your models here.
-class Person(models.Model):
-    name = models.TextField()
-    age = models.IntegerField()
-    sex = models.TextField()
+class Employee(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=255)
+    department_id = models.IntegerField()
+    job_title = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    hire_date = models.DateField()
+
+    class Meta:
+        db_table = "auditdb.employees"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Record(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -19,4 +35,4 @@ class Record(models.Model):
     zipcode = models.CharField(max_length=50)
 
     def __str__(self):
-        return(f"{self.first_name} {self.last_name}")
+        return f"{self.first_name} {self.last_name}"
